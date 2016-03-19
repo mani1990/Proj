7e9c08a4ed2308514d37c299c9b765780412b2b7
pallavi.maheshwari <mpallavibajaj@gmail.com>
2016-03-19 23:24:17 +0530

    first commit

---
+++ UpdateOrMerge/src/in/co/sunrays/Mobile.java
@@ -0,0 +1,30 @@
+package in.co.sunrays;
+
+public class Mobile {
+	
+	private long id;
+	private String name;
+	private String color;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+
+}
